320f6d79b4d0be9e6add34a6893bb06d4cf0a34a
Natasha S Love <debora@inbox.ru>
2018-04-25 19:18:05 -0500

    partially done

--- CIS2013_Week14_Quiz1.cpp
+++ CIS2013_Week14_Quiz1.cpp
@@ -2,9 +2,19 @@
 #include <cstring>
 using namespace std;
 //int number = 100;
+typedef unsigned char* ByteArray;
 
 class Game {
 	public:
+	
+	Game () {
+		setName ("Minesweeper");
+		 // construct - returns the function after creating an object
+		getName ();
+		setScore ();
+		getScore ();
+						
+		}
 	
 	void setName(string b)
 	{
@@ -14,21 +24,149 @@ class Game {
 	string getName() {
 		return (name);
 	}
+	int setScore () {
+		score++;
+	}
+	int getScore () {
+		return score;
+	}
 	
 	private:
 	string name;
-	int score;
+	int score=0;
 };
+
+class MineSweeper: public Game {
+	public:
+	int mine;
+	int x, y;// rows = x; columns= y;
+	int sel_sq;
+	int bomb_sq;
+	ByteArray *m = new ByteArray[y];
+	
+	MineSweeper(): Game () {
+	setBoard ();
+	setMines ();
+	
+	}
+	
+	
+
+	void setBoard () {
+		cout << ' ' << endl;
+		cout << "Enter the board size :\n";
+		cout<< "x =";
+		cin >> x;
+		cout << endl;
+		cout << "y =";
+		cin >> y;
+		cout<< endl;
+		
+		for (int i = 0; i < y; i++) {
+			m[i] = new unsigned char[x];
+			//m is now a y by x array. x - width, y - height
+		}
+			
+		for (int i = 0; i < y; i++){
+			for (int j = 0;j < x; j++)
+			{
+				m[i][j] = '.';
+			}
+		}
+	}
+	
+	void setMines () {
+		cout << "Enter amount of mines (not more than 10)for your field: " << endl;
+		cin >> mine;
+		cout << endl;
+		cout << mine << " mines are..."; 
+
+	int* mines = new int[mine*2];// double, because we need 2 coordinates for each mine
+	int mx, my; // coordinates
+	
+	for (int n = 0; n < mine*2; n+=2)
+	{
+		mx = rand() % x;
+		my = rand() % y;	
+
+		while (1) {
+			 int i; 
+			 
+			 for (i = 0; i < n; i+=2) { 
+			 
+			 //if mine already placed here -  randomize one more time
+			 if (mines[i] == mx && mines[i+1] == my) // coordinates go after each other in the array
+				
+				{ mx = rand() % x; 
+				  my = rand() % y; 
+				  break; 
+				} 
+			} 
+			if (i == n) //all mines are planted - get out of the loop
+			break; 
+		} 
+		
+		// coordinates of mines go after each other in the array
+		 mines[n] = mx; 
+		 mines[n + 1] = my;
+		
+	}
+	cout << " ...planted in the play board" << endl; 
+	cout << endl;
+	}
+	
+	 void print(unsigned char** board, int x, int y){
+
+		cout << '\t';
+		for (int a = 0; a < x; a++)
+		{
+			cout << a << ' ';
+		}
+		cout << '\n';
+		
+		for (int i = 0; i < y; i++)
+		{
+			cout << i << '\t';
+			for (int j = 0; j < x;j++)
+			{
+				cout << board[i][j] << ' ';
+			}
+			cout << '\n';
+		}
+		cout<< endl;
+	}
+	
+	void getSquare () {
+		
+		cout << "Let's play! Enter coordinates : "<<endl; 
+		 
+		 cout << "x ="; 
+		 cin >> x;
+		 cout << endl;
+		 cout << "y =";
+		 cin >> y;
+		 cout << endl; 
+	}
+};
+
 
 int main(){
 	string n;
-	Game game;
+	unsigned char m;
+	int x,y;
+	//Game game;
 	cout << " What is the game you wanna play? ";
 	cin>> n;
+	MineSweeper minesweeper;
+	minesweeper.setName (n);
 	
-	game.setName (n);
 	
-	cout << "Playing " << game.getName () << endl;
+	cout << "Playing " << minesweeper.getName () << endl;
+	//game.setScore ();
+	
+	//cout << "your score is " << game.getScore () << endl;
+	minesweeper.print(m, x, y);
+	
 	
 	return 0;
 }
